1bce5a82186a81972812712f1ed536ccba421c76
PhamVanNhi <vannhibka@gmail.com>
2014-05-06 16:49:06 +0700

    Create Caculate the square root.c

---
+++ Caculate the square root.c
@@ -0,0 +1,27 @@
+/*----------Desciption-----------------------------------
+
+Enter a number from keyboard to th screen.
+   +If that number is negative, caculate the square root.
+   +Conversely, Error and exit th program.
+---------------------------------------------------------*/
+/*	Author:___Jacob_Pham______
+	Tools:____C-Free_5.0______
+----------------------------------------------------------*/
+
+#include <conio.h>
+#include <stdio.h>
+#include <math.h>
+
+main()
+{
+	double n, canbac2;
+	printf("Nhap mot so nguyen: n = ");
+	scanf("%lf",&n);
+	if(n>=0)
+	{
+		canbac2 = sqrt(n);
+		printf("Can bac hai cua %lf la %lf\n",n,canbac2);
+	}
+	else
+		printf("Error !\n");
+}
